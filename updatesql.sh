3e8cce9b91960d936568f092ace50e9bf0ce0b4f
SPOOCQ <mailbox@spoocq.ga>
2020-05-05 16:42:36 +0200

    Create updatesql.sh

---
+++ updatesql.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+### Script to check for SQL changes in a git repository using a gitlab-runner
+
+##### Usage ######
+# Add it to your .gitlab-ci.yml like this:
+#
+#job_deploy_master:
+#  stage: deploy
+#  only:
+#    - master
+#  script:
+#    - ~/sql_build $CI_COMMIT_BEFORE_SHA $COMMIT_SHA
+#  tags:
+#    - php
+#
+##################
+OLD=$1
+CURRENT=$2
+#####
+SQL_PATH="WHERE_THE_UPDATE_SQL_FILE_SHOULD_BE_PLACED_AND_FILENAME"
+SQL_DB="YOUR_DATABASE"
+SQL_USER="YOUR_DBUSERNAME"
+SQL_PASS="YOUR_DBPASSWORD"
+
+
+build_updsql() {
+        rm $SQL_PATH
+        touch $SQL_PATH
+        mapfile -t changes < <(git diff --name-only $OLD $CURRENT sql/)
+        for element in "${changes[@]}"
+                do
+                        cat "$element" > $SQL_PATH
+                done
+}
+
+
+if [ "$OLD" == "$CURRENT" ]; then
+        echo "Nothing to be done"
+else
+        build_updsql
+        if [ -s $SQL_PATH ]
+        then
+                mysql -u $SQL_USER -p$SQL_PASS $SQL_DB < $SQL_PATH
+        else
+                echo "Nothing to be done"
+        fi
+fi
