aec6db5e9214d8b5bf48f615234871abe7d461f1
SPOOCQ <mailbox@spoocq.ga>
2020-05-20 12:56:05 +0200

    Update updatesql.sh
    fixed missing >, added DB dump

--- updatesql.sh
+++ updatesql.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ### Script to check for SQL changes in a git repository using a gitlab-runner
-
+##### TODO: Add a way to revert the changes to a specific hash using commandline paramaters
 ##### Usage ######
 # Add it to your .gitlab-ci.yml like this:
 #
@@ -18,6 +18,7 @@ OLD=$1
 CURRENT=$2
 #####
 SQL_PATH="WHERE_THE_UPDATE_SQL_FILE_SHOULD_BE_PLACED_AND_FILENAME"
+SQL_PATH="WHERE_DB_DUMPS_SHOULD_BE_SAVED"
 SQL_DB="YOUR_DATABASE"
 SQL_USER="YOUR_DBUSERNAME"
 SQL_PASS="YOUR_DBPASSWORD"
@@ -29,7 +30,7 @@ build_updsql() {
         mapfile -t changes < <(git diff --name-only $OLD $CURRENT sql/)
         for element in "${changes[@]}"
                 do
-                        cat "$element" > $SQL_PATH
+                        cat "$element" >> $SQL_PATH
                 done
 }
 
@@ -40,7 +41,11 @@ else
         build_updsql
         if [ -s $SQL_PATH ]
         then
+                echo "Dumping existing database..."
+                mysqldump -u $SQL_USER -p$SQL_PASS --databases $SQL_DB --result-file=$DUMP_PATH/$OLD.sql 
+                echo "Updating database...
                 mysql -u $SQL_USER -p$SQL_PASS $SQL_DB < $SQL_PATH
+                echo "All done"
         else
                 echo "Nothing to be done"
         fi
